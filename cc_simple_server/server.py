f3eda82b3af0a4fa7e68cb9cebff378440c52267
Brandon Moretti <brm236@pitt.edu>
2025-01-29 22:06:14 -0500

    THIS IS IT

--- cc_simple_server/server.py
+++ cc_simple_server/server.py
@@ -122,5 +122,5 @@ async def delete_task(task_id: int):
     conn.commit()
     cursor = conn.cursor()
     conn.close()
-    return ["The task was deleted successfully."]
+    return [f"Task {task_id} was deleted successfully."]
     raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
